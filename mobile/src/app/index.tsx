f1243bbeb4c00c2a8182f0fabf731cdfdc2a277a
ViniciusMarcuS2i <marcusononato@gmail.com>
2024-12-16 17:50:12 -0400

    feat: add some components from gluestack

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -4,6 +4,9 @@ import { Redirect } from "expo-router";
 import { Header } from "../components/header";
 import { HomeAnnouncementCard } from "../components/home-announcement-card";
 import { Text } from "../components/ui/text";
+import { SearchBar } from "../components/search-bar";
+import { ProductItem } from "../components/product-item";
+import { ScrollView } from "react-native";
 
 function Home() {
   const [isAuth, setIsAuth] = useState(true);
@@ -13,12 +16,16 @@ function Home() {
   }
 
   return (
-    <VStack className="flex-1 px-6 pt-16">
+    <VStack className="flex-1 bg-gray-600 px-6 pt-16">
       <Header />
       <VStack className="mt-10">
         <HomeAnnouncementCard />
       </VStack>
-      <Text className="mt-6 text-lg">Compre produtos variados</Text>
+      <Text className="mb-3 mt-6 text-lg">Compre produtos variados</Text>
+      <SearchBar />
+      <ScrollView className="mt-8">
+        <ProductItem />
+      </ScrollView>
     </VStack>
   );
 }
