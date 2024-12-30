d8b349b2a52fa03639731f3ea91155d4b2334abe
ViniciusMarcuS2i <marcusononato@gmail.com>
2024-12-30 17:45:10 -0400

    feat: removed product item avatar

--- mobile/src/components/product-item.tsx
+++ mobile/src/components/product-item.tsx
@@ -47,13 +47,6 @@ export function ProductItem({ product }: ProductProps) {
               }}
               imageClassName="rounded-lg"
             >
-              <Avatar className="h-10 w-10">
-                <AvatarImage
-                  source={{
-                    uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-                  }}
-                />
-              </Avatar>
               <VStack>
                 <Badge className="rounded-lg bg-gray-200 px-3">
                   <BadgeText
